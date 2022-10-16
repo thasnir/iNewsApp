cae0c9e045911c631771a43fe952c7ac7158b77a
thasni.r <thasni444@gmail.com>
2022-10-17 00:29:57 +0530

    bug fixes

--- app/src/androidTest/java/com/cba/inewsapp/NewsFragmentTest.kt
+++ app/src/androidTest/java/com/cba/inewsapp/NewsFragmentTest.kt
@@ -2,6 +2,7 @@ package com.cba.inewsapp
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.LargeTest
@@ -31,16 +32,32 @@ class NewsFragmentTest {
     @Before
     fun init(){
         hiltRule.inject()
-        //launchF
+    }
+    @Test
+    fun newsListTest(){
+        val list=Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.lvNews)
+            )
+        )
+        list.check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+         list.perform(click())
     }
     @Test
     fun newsFeedTest(){
         sleep(1000)
-        val tlNews =Espresso.onView(
+        val desc =Espresso.onView(
             Matchers.allOf(
                 ViewMatchers.withId(R.id.newHeading)
             )
         )
-        tlNews.check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        desc.check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        val title =Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.newsTime)
+            )
+        )
+        title.check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
--- app/src/main/java/com/cba/inewsapp/MainActivity.kt
+++ app/src/main/java/com/cba/inewsapp/MainActivity.kt
@@ -2,18 +2,20 @@ package com.cba.inewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.cba.inewsapp.R
+import com.cba.inewsapp.databinding.ActivityMainBinding
+import com.cba.inewsapp.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        navController.navigate(R.id.newsFragment)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
